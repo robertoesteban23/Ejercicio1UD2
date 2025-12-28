14479479267bd4b5fbb998bab4439d4f63d4cb62
roberto <robertillo.tablet@gmail.com>
2025-12-28 14:20:11 +0100

    Proyecto2 añadido sin bin

---
+++ Proyecto2/src/Sumando/Sumando.java
@@ -0,0 +1,18 @@
+package Sumando;
+
+import java.util.Scanner;
+
+public class Sumando {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	Scanner scanner = new Scanner (System.in);
+	
+	System.out.println("Introduzca numero1: ");
+	int num1 = scanner.nextInt();
+	System.out.println("Introduzca numero2: ");
+	int num2= scanner.nextInt();
+	System.out.println(num1 + num2);
+	}
+
+}
